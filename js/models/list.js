db9adc1a6633f08d67ee465ecfd33baf225cc5e1
github username <github email address>
2017-07-21 23:52:24 -0400

    Done.

--- js/models/list.js
+++ js/models/list.js
@@ -1,2 +1,14 @@
 // List Model
 
+let counter = 0
+class List {
+  constructor(title){
+    this.title = title
+    this.id = counter++
+    this.tasks = []
+    List.all.push(this)
+  }
+
+};
+
+List.all = []
--- js/models/task.js
+++ js/models/task.js
@@ -1,2 +1,15 @@
 // Task Model
+let count = 0
 
+class Task {
+  constructor(description, priority, list){
+    this.description = description;
+    this.priority = priority;
+    this.id = count++ - 2;
+    this.list = list;
+  }
+
+  list(){
+    List.all
+  }
+}
